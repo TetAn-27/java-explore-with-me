39e8e9806eb7fe5fb13cbe365d2ed72ddd90d29f
TetAn <tetan27@yandex.ru>
2023-09-11 20:49:42 +0700

    feat: Внесение необходимых правок

--- stats/server/src/main/java/ru/practicum/stat/StatsMain.java
+++ stats/server/src/main/java/ru/practicum/stat/StatsMain.java
@@ -2,7 +2,9 @@ package ru.practicum.stat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("ru.practicum.")
 @SpringBootApplication
 public class StatsMain {
     public static void main(String[] args) {
