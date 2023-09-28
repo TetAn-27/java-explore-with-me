cdcabe98bdefded0cd46a1af4dd6d0a758595be1
TetAn <tetan27@yandex.ru>
2023-09-28 22:09:43 +0700

    feat: Создать модель сущности и DTO comment

---
+++ service/src/main/java/ru/practicum/comments/dto/CommentDtoForGet.java
@@ -0,0 +1,20 @@
+package ru.practicum.comments.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDtoForGet {
+    private long id;
+    private String text;
+    private long eventId;
+    private long authorId;
+    private String authorName;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdDate;
+}
